e0f42b632688e01bfff4409373a03a7e7a421728
Shraddha <160100827+Shraddha318@users.noreply.github.com>
2025-01-04 01:44:01 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
-// import logo from './logo.svg';
+import React from 'react';
 import './App.css';
-import AllRoutes from './Routes/AllRoutes';
-import Navbar from './Routes/Navbar';
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import Blog from './pages/Blog';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+
 function App() {
   return (
     <div className="App">
-      <Navbar/>
-      <AllRoutes/>
+      {/* Navbar component to handle navigation */}
+      <Navbar />
+      
+      <main>
+        <Routes>
+          {/* Define routes for different pages */}
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/blog" element={<Blog />} />
+        </Routes>
+      </main>
+      
+      {/* Footer component */}
+      <Footer />
     </div>
   );
 }
