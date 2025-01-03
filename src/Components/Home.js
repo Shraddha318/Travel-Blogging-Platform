6cd51ad7a41ec90b7acbf624830aef729e7fd0b4
Shraddha <160100827+Shraddha318@users.noreply.github.com>
2025-01-04 01:46:49 +0530

    Create Home.js

---
+++ src/Components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <div>
+      <h1>Welcome to the Travel Blogging Platform</h1>
+      <p>Discover amazing travel destinations and tips!</p>
+    </div>
+  );
+}
+
+export default Home;
