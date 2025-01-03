7b8d6e3987b5b85e9fa5c224157ef21482bd4838
Shraddha <160100827+Shraddha318@users.noreply.github.com>
2025-01-04 01:45:42 +0530

    Create Footer.js

---
+++ src/Components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer>
+      <p>© 2025 Travel Blogging Platform. All rights reserved.</p>
+    </footer>
+  );
+}
+
+export default Footer;
